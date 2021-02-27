4e4b1fe1f93a4e692159ed07def09ac09befbfea
Khofidin <offiedz@gmail.com>
2021-02-27 23:16:31 +0700

    Add detail view with grid

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:splash_gram/data/response_unsplash.dart';
+import 'package:splash_gram/detail_screen.dart';
 
 import 'data/api.dart';
 
@@ -25,60 +26,71 @@ class _State extends State<MainScreen> {
               return ListView.builder(
                 itemBuilder: (BuildContext context, int index) {
                   var unsplash = snapshot.data.data[index];
-                  return Card(
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) {
+                        return DetailScreen(
+                          unsplash: unsplash,
+                          data: snapshot.data.data,
+                        );
+                      }));
+                    },
+                    child: Card(
                       child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            CircleAvatar(
-                              backgroundImage: NetworkImage(unsplash.user.profileImage.medium),
-                              radius: 28,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(4.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    unsplash.user.name,
-                                    style: Theme.of(context).textTheme.headline6,
-                                    overflow: TextOverflow.fade,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                CircleAvatar(
+                                  backgroundImage: NetworkImage(unsplash.user.profileImage.medium),
+                                  radius: 28,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        unsplash.user.name,
+                                        style: Theme.of(context).textTheme.headline6,
+                                        overflow: TextOverflow.fade,
+                                      ),
+                                      Text(
+                                        unsplash?.location?.city ?? "Unknown Location",
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 1,
+                                        softWrap: false,
+                                      )
+                                    ],
                                   ),
-                                  Text(
-                                    unsplash?.location?.city ?? "Unknown Location",
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 1,
-                                    softWrap: false,
-                                  )
-                                ],
-                              ),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
+                          ),
+                          Image.network(unsplash.urls.regular),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              "${unsplash.likes.toString()} likes",
+                              textAlign: TextAlign.end,
+                              style: Theme.of(context).textTheme.subtitle1,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(DateFormat('dd MMMM yyyy').format(unsplash.createdAt)),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(unsplash?.description ?? ''),
+                          )
+                        ],
                       ),
-                      Image.network(unsplash.urls.regular),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          "${unsplash.likes.toString()} likes",
-                          textAlign: TextAlign.end,
-                          style: Theme.of(context).textTheme.subtitle1,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(DateFormat('dd MMM yyyy').format(unsplash.createdAt)),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(unsplash?.description ?? ''),
-                      )
-                    ],
-                  ));
+                    ),
+                  );
                 },
                 itemCount: snapshot.data.data.length,
               );
